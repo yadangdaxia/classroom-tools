e36b731c2e5cc2dc44798a369de25b314fde3e5b
Adam Matteson <matteson211@hotmail.com>
2022-03-15 22:36:26 +0800

    need to make into list

--- main.js
+++ main.js
@@ -6,16 +6,30 @@ form.addEventListener('submit', (e) => {
 
   const studentString = document.getElementById('random-names').value
   const studentArray = studentString.split(',')
-  console.log(studentArray)
+  // console.log(studentArray)
   const mixedArray = mix(studentArray)
-  console.log(mixedArray)
+  // console.log(mixedArray)
 
   const displayArea = document.getElementById('random-order-result');
   console.log(displayArea)
-  displayArea.textContent = mixedArray
+
+  // displayArea.textContent = mixedArray
+  let listItem = document.createElement('li');
+  console.log(listItem, "listitem")
+  mixedArray.forEach((student) => {
+    console.log(student, "list of students shoudl be here")
+    displayArea.appendChild(listItem);
+    listItem.textContent = student
+
+  } )
+    // for item in array, add new li
+
 })
 
 
+
+// square.classList.add('square')
+// square.textContent = 'click to change color'
 
 
 
